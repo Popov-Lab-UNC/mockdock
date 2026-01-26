65d3af9bd7c2fc9d4079fd60105abc099c2cf8c3
Jason Lin <jasonlin90125@gmail.com>
2026-01-26 16:57:15 -0500

    Reorg

--- fcgmb/__init__.py
+++ fcgmb/__init__.py
@@ -1,14 +1,24 @@
 from .data import fetch_chembl_data
-from .docking import AutoDockGPUOracle
+from .docking import AutoDockGPUOracle, DockingOracle
 from .receptor import ReceptorPreparer, extract_protein_and_ligand
-from .utils import plot_docking_results, plot_activity_distribution, fetch_ligand_expo_sdf, assign_bond_orders_from_template
+from .ligand_prep import LigandPreparer
+from .analysis import DockingAnalyzer
+from .utils import (
+    plot_docking_results, 
+    plot_activity_distribution, 
+    fetch_ligand_expo_sdf, 
+    assign_bond_orders_from_template
+)
 from .oracle import FCGMBOracle
 
 __all__ = [
     "fetch_chembl_data",
     "AutoDockGPUOracle",
+    "DockingOracle",
     "ReceptorPreparer",
     "extract_protein_and_ligand",
+    "LigandPreparer",
+    "DockingAnalyzer",
     "plot_docking_results",
     "plot_activity_distribution",
     "fetch_ligand_expo_sdf",
--- validation_test.py
+++ validation_test.py
@@ -7,8 +7,8 @@ def run_validation():
     # The 3 best benchmarks
     benchmarks = [
         "CHEMBL4482_5XVA_CHEMBL4028914",
-        "CHEMBL4630_2R0U_CHEMBL1140535",
-        "CHEMBL3313835_7KCF_CHEMBL4834423"
+        #"CHEMBL4630_2R0U_CHEMBL1140535",
+        #"CHEMBL3313835_7KCF_CHEMBL4834423"
     ]
     
     # Path to AutoDock-GPU
